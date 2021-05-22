24befe228b0968887ab6b9104d557341f57be10f
IlianaXn <el17059@mail.ntua.gr>
2021-05-23 01:16:14 +0300

    Update .gitignore

--- back-end/SOA/QnAOperationsService/controllers/answer.js
+++ back-end/SOA/QnAOperationsService/controllers/answer.js
@@ -7,13 +7,17 @@ module.exports = {
         const createdAns = await AScreate(req.body);
         return res.send(createdAns);
       }
-      catch (err => next(err));
-    },
+      catch (err) {
+          next(err);
+      }
+      },
     async getall(req,res,next){
       try {
         const returnedAns = await ASget(req.params.questionId);
         return res.send(returnedAns);
       }
-      catch (err => next(err));
+      catch (err) {
+          next(err);
+      }
     }
 }
--- back-end/SOA/QnAOperationsService/controllers/question.js
+++ back-end/SOA/QnAOperationsService/controllers/question.js
@@ -7,20 +7,26 @@ module.exports = {
         const createdQues = await QScreate(req.body);
         return res.send(createdQues);
       }
-      catch (err => next(err));
+      catch (err) {
+          next(err);
+      }
     },
     async getall(req, res, next){
       try {
         const returnedQues = await QSgetall();
         return res.send(returnedQues);
       }
-      catch (err => next(err));
-    },
+      catch (err) {
+          next(err);
+      }
+      },
     async getfiltered(req, res, next){
       try {
         const returnedQues = await QSgetfiltered(req.params);
         return res.send(returnedQues);
       }
-      catch (err => next(err));
+      catch (err) {
+          next(err);
+      }
     }
   }
