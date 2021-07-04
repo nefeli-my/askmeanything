8735b6435484b7ae34908fae7740b27775d02d61
IlianaXn <el17059@mail.ntua.gr>
2021-07-04 09:06:33 +0300

    Hide email id

--- front-end-micro/src/components/ContactForm.js
+++ front-end-micro/src/components/ContactForm.js
@@ -22,7 +22,7 @@ const ContactForm = () => {
             'askmeanything',
             'contactus',
             toSend,
-            'user_6RYYMLMUvxyILBTlAHQxV'
+            process.env.EMAIL_ID
         )
             .then((response) => {
                 NotificationManager.success('Your message was sent successfully!','Success!', 2000);
